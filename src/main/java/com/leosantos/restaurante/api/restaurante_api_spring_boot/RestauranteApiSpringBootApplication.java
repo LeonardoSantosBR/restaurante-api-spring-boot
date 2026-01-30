76182593921d587e22240a4996032bb065e87e51
LeonardoSantosBR <leosantosmelo07@gmail.com>
2026-01-30 13:42:58 -0300

    init: inicio api

---
+++ src/main/java/com/leosantos/restaurante/api/restaurante_api_spring_boot/RestauranteApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.leosantos.restaurante.api.restaurante_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leosantos/restaurante/api/restaurante_api_spring_boot/RestauranteApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leosantos.restaurante.api.restaurante_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
